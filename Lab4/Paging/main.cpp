4ccc371c54fd6d23292540dbe2a7311cceef1373
Chrome <chrome@127.179.233.68.naturalwireless.com>
2017-04-20 01:22:06 -0400

    finished lifo and random: output is not done

--- Lab4/Paging/main.cpp
+++ Lab4/Paging/main.cpp
@@ -1,6 +1,71 @@
-#include <iostream>
+#include "paging.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char const *argv[]) {
+    if (argc == 8) {
+        int M = atoi(argv[1]);  // the machine size in words
+        int P = atoi(argv[2]);  // the page size in words
+        int S = atoi(argv[3]);  // the size of a process
+        int J = atoi(argv[4]);  // the ‘‘job mix’’, which determines A, B, and C
+        int N = atoi(argv[5]);  // the number of references for each process
+        std::string R = argv[6];  // LIFO, RANDOM, or LRU
+        int debugging = atoi(argv[7]);
+        // initiate frame table
+        std::vector<Frame> frame_table;
+        int frame_number = M / P;
+        for (int i = 0; i < frame_number; i++) {
+            Frame tmp_f;
+            frame_table.push_back(tmp_f);
+        }
+        // initiate Process vector
+        std::vector<Process> processes;
+        if (J == 1) {
+            Process tmp_p(1, 0, 0, S, N, 111 % S);
+            processes.push_back(tmp_p);
+        }
+        else if (J == 2) {
+            for (int i = 0; i < 4; i++) {
+                Process tmp_p(1, 0, 0, S, N, 111 * (i + 1) % S);
+                processes.push_back(tmp_p);
+            }
+        }
+        else if (J == 3) {
+            for (int i = 0; i < 4; i++) {
+                Process tmp_p(1, 0, 0, S, N, 111 * (i + 1) % S);
+                processes.push_back(tmp_p);
+            }
+        }
+        else if (J == 4) {
+            Process tmp_p1(0.75, 0.25, 0, S, N, 111 % S);
+            Process tmp_p2(0.75, 0, 0.25, S, N, 222 % S);
+            Process tmp_p3(0.75, 0.125, 0.125, S, N, 333 % S);
+            Process tmp_p4(0.5, 0.125, 0.125, S, N, 444 % S);
+            processes.push_back(tmp_p1);
+            processes.push_back(tmp_p2);
+            processes.push_back(tmp_p3);
+            processes.push_back(tmp_p4);
+        }
+        // do the process
+        std::cout << "Process" << std::endl;
+        FILE * pFile;
+        pFile = fopen ("random-numbers.txt" , "r");
+        if (pFile == NULL) {
+            perror ("Error opening file");
+            return 0;
+        }
+        if (debugging == 0) {
+            if (R.compare("lifo") == 0) {
+                LIFO(pFile, frame_table, frame_number, processes, P);
+            }
+            else if (R.compare("random") == 0) {
+                RANDOM(pFile, frame_table, frame_number, processes, P);
+            }
+            else if (R.compare("lru") == 0) {
+                LRU(pFile, frame_table, frame_number, processes, P);
+            }
+        }
+        fclose (pFile);
+    } else {
+        std::cout << "Wrong arguments." << std::endl;
+    }
     return 0;
 }
--- Lab4/Paging/paging.h
+++ Lab4/Paging/paging.h
@@ -5,4 +5,245 @@
 #ifndef PAGING_PAGING_H
 #define PAGING_PAGING_H
 
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <sstream> // read file as standard input
+#include <vector>
+#include <deque>
+#include <queue> // std::priority_queue
+#include <algorithm>
+#include <limits>
+#include <iostream>
+
+class Frame {
+    int process_index, page_index;
+public:
+    Frame(int process_i = -1, int page_i = -1) {
+        process_index = process_i;
+        page_index = page_i;
+    }
+    int get_PrI() const { return process_index; }
+    int get_PI() const { return page_index; }
+    bool find_Frame(int process_i, int page_i) {
+        return (process_i == process_index && page_i == page_index);
+    }
+    bool find_FrameByProcessIndex(int process_i) {
+        return (process_i == process_index);
+    }
+    bool is_free() {
+        return (process_index == -1 && page_index == -1);
+    }
+    void change_Frame(int process_i, int page_i) {
+        process_index = process_i;
+        page_index = page_i;
+    }
+    void clear() {
+        process_index = -1;
+        page_index = -1;
+    }
+};
+
+class Process {
+    double A, B, C;
+    int S, number_of_references, reference_word;
+    bool complete;
+public:
+    Process(double a, double b, double c, int s, int n, int r, bool complete = false) {
+        A = a;
+        B = b;
+        C = c;
+        S = s;
+        number_of_references = n;
+        reference_word = r;
+    }
+    double get_A() const { return A; }
+    double get_B() const { return B; }
+    double get_C() const { return C; }
+    int get_S() const { return S; }
+    int get_N() const { return number_of_references; }
+    int get_R() const { return reference_word; }
+    void minus_one_N() { number_of_references -= 1; }
+    void change_R(int new_r) { reference_word = new_r; }
+    void set_complete() { complete = true; }
+    bool is_complete() { return complete; }
+};
+
+int randomNextReference(double A, double B, double C, int S, int w, FILE *pFile) {
+    double y;
+    unsigned int r;
+    if ( ! feof (pFile) )
+        if ( fscanf (pFile , "%i" , &r) != EOF ) {
+            std::cout << "random number: " << r << std::endl;
+            y = r / (INT_MAX + 1.0);
+            if (y < A) {
+                return (w + 1) % S;
+            }
+            else if (y < (A + B)) {
+                return (w - 5 + S) % S;
+            }
+            else if (y < (A + B + C)) {
+                return (w + 4) % S;
+            }
+            else { // a random value in 0..S-1 each with probability (1-A-B-C)/S
+            }
+        }
+    return 0;
+}
+
+int randomEvictingFrame(FILE *pFile, int frame_number) {
+    unsigned int r;
+    if ( ! feof (pFile) )
+        if ( fscanf (pFile , "%i" , &r) != EOF ) {
+            std::cout << "random number: " << r << std::endl;
+            return (r % frame_number);
+        }
+    return 0;
+}
+
+bool is_empty(std::vector<Process> processes) {
+    for (int i = 0; i < processes.size(); i++) {
+        if (!processes[i].is_complete()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void LIFO(FILE *pFile, std::vector<Frame> frame_table, int frame_number, std::vector<Process> processes, int P) {
+    int q = 3;
+    int time_count = 0;
+    while (!is_empty(processes)) {
+        for (int i = 0; i < processes.size(); i++) {
+            for (int ref = 0; ref < q; ref++) {
+                time_count++;
+                // Part 1:
+                //   simulate this reference for this process
+                bool hit = false;
+                int process_i = i + 1;
+                int page_i = processes[i].get_R() / P;
+                // find the page in the frame table
+                for (int j = 0; j < frame_table.size(); j++) {
+                    if (frame_table[j].find_Frame(process_i, page_i)) {
+                        hit = true;
+                        std::cout << process_i << " references word " << processes[i].get_R()
+                                  << " (page " << page_i << ") at time " << time_count << ": "
+                                  << "Hit in frame " << j << std::endl;
+                        break;
+                    }
+                }
+                // if not found
+                if (!hit) {
+                    // if free frame exists
+                    bool free = false;
+                    for (int k = frame_number; k >= 0; k--) {
+                        if (frame_table[k].is_free()) {
+                            frame_table[k].change_Frame(process_i, page_i);
+                            free = true;
+                            std::cout << process_i << " references word " << processes[i].get_R()
+                                      << " (page " << page_i << ") at time " << time_count << ": "
+                                      << "Fault, using free frame " << k << std::endl;
+                            break;
+                        }
+                    }
+                    // if free frame doesn't exist, then pick a victim
+                    if (!free) {
+                        std::cout << process_i << " references word " << processes[i].get_R()
+                                  << " (page " << page_i << ") at time " << time_count << ": "
+                                  << "Fault, evicting page " << frame_table[0].get_PI()
+                                  << " of " << frame_table[0].get_PrI() << " from frame 0" << std::endl;
+                        frame_table[0].change_Frame(process_i, page_i);
+                    }
+                }
+                // Part 2:
+                //   calculate the next reference for this process
+                processes[i].change_R(randomNextReference(processes[i].get_A(),
+                                                          processes[i].get_B(),
+                                                          processes[i].get_C(),
+                                                          processes[i].get_S(),
+                                                          processes[i].get_R(),
+                                                          pFile));
+                // if the process is done
+                processes[i].minus_one_N();
+                if (processes[i].get_N() == 0) {
+                    processes[i].set_complete();
+                    break;
+                }
+            }
+        }
+    }
+
+}
+
+void RANDOM(FILE *pFile, std::vector<Frame> frame_table, int frame_number, std::vector<Process> processes, int P) {
+    int q = 3;
+    int time_count = 0;
+    while (!is_empty(processes)) {
+        for (int i = 0; i < processes.size(); i++) {
+            for (int ref = 0; ref < q; ref++) {
+                time_count++;
+                // Part 1:
+                //   simulate this reference for this process
+                bool hit = false;
+                int process_i = i + 1;
+                int page_i = processes[i].get_R() / P;
+                // find the page in the frame table
+                for (int j = 0; j < frame_table.size(); j++) {
+                    if (frame_table[j].find_Frame(process_i, page_i)) {
+                        hit = true;
+                        std::cout << process_i << " references word " << processes[i].get_R()
+                                  << " (page " << page_i << ") at time " << time_count << ": "
+                                  << "Hit in frame " << j << std::endl;
+                        break;
+                    }
+                }
+                // if not found
+                if (!hit) {
+                    // if free frame exists
+                    bool free = false;
+                    for (int k = frame_number; k >= 0; k--) {
+                        if (frame_table[k].is_free()) {
+                            frame_table[k].change_Frame(process_i, page_i);
+                            free = true;
+                            std::cout << process_i << " references word " << processes[i].get_R()
+                                      << " (page " << page_i << ") at time " << time_count << ": "
+                                      << "Fault, using free frame " << k << std::endl;
+                            break;
+                        }
+                    }
+                    // if free frame doesn't exist, then pick a victim
+                    if (!free) {
+                        int index = randomEvictingFrame(pFile, frame_number);
+                        std::cout << process_i << " references word " << processes[i].get_R()
+                                  << " (page " << page_i << ") at time " << time_count << ": "
+                                  << "Fault, evicting page " << frame_table[index].get_PI()
+                                  << " of " << frame_table[index].get_PrI() << " from frame " << index << std::endl;
+                        frame_table[index].change_Frame(process_i, page_i);
+                    }
+                }
+                // Part 2:
+                //   calculate the next reference for this process
+                processes[i].change_R(randomNextReference(processes[i].get_A(),
+                                                          processes[i].get_B(),
+                                                          processes[i].get_C(),
+                                                          processes[i].get_S(),
+                                                          processes[i].get_R(),
+                                                          pFile));
+                // if the process is done
+                processes[i].minus_one_N();
+                if (processes[i].get_N() == 0) {
+                    processes[i].set_complete();
+                    break;
+                }
+            }
+        }
+    }
+
+}
+
+void LRU(FILE *pFile, std::vector<Frame> frame_table, int frame_number, std::vector<Process> processes, int P) {
+
+}
+
 #endif //PAGING_PAGING_H
